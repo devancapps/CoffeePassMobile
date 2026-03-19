5d64c0fc5a2c1549b71199f3a9a1a620512c1a4a
Tawny Motoyama <tamotoyama@gmail.com>
2026-03-18 22:56:35 -0500

    install babel-preset-expo and modify babel config for expo bundler

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    // Temporary workaround: Expo's preset runtime polyfill path is erroring
+    // under the current Node/tooling combination ("lodash.debounce is not a function").
+    // Disabling @babel/plugin-transform-runtime unblocks Metro bundling.
+    presets: [['babel-preset-expo', { enableBabelRuntime: false }]],
     plugins: [
       [
         'module-resolver',
